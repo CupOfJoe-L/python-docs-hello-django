a55e5b9adf6999237e3562afb4a68718df1f5560
CupOfJoe-L <jjoeliang@gmail.com>
2021-07-15 10:54:06 +0200

    Extended message further

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World! And everyone out there!")
+    return HttpResponse("Hello, World! And everyone out there! Great, now I know it's not bork.")
